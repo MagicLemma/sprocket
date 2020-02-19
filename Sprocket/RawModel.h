03cc4489f698ac2ba0d5cce7bacd7cb5026f4e61
Matthew-TC <18335242+MattMorphic@users.noreply.github.com>
2020-02-19 20:52:50 +0000

    Moved RawModel up a dir

---
+++ Sprocket/RawModel.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace Sprocket {
+
+class RawModel
+{
+    unsigned int d_vaoId;
+    int          d_vertexCount;
+
+public:
+    RawModel(unsigned int vaoId, int vertexCount);
+
+    unsigned int vaoId() const { return d_vaoId; }
+    int vertexCount() const { return d_vertexCount; }
+
+    void bind() const;
+    void unbind() const;
+};
+
+}
