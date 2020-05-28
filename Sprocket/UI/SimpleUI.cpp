66359f6eb2cdf0a82aba0cb52e95371540ff33c1
Matthew-TC <18335242+MattMorphic@users.noreply.github.com>
2020-05-29 00:45:21 +0100

    Create the basics of SimpleUI::Context

--- Sprocket/UI/SimpleUI.cpp
+++ Sprocket/UI/SimpleUI.cpp
@@ -1,5 +1,33 @@
 #include "SimpleUI.h"
+#include "KeyboardProxy.h"
+#include "MouseProxy.h"
 
 namespace Sprocket {
+namespace SimpleUI {
 
+struct SimpleUIData
+{
+    Window* window;
+
+    KeyboardProxy keyboard;
+    MouseProxy mouse;
+};
+
+Context::Context(Window* window)
+    : d_impl(std::make_shared<SimpleUIData>())
+{
+}
+
+void Context::handleEvent(Event& event)
+{
+    d_impl->keyboard.handleEvent(event);
+    d_impl->mouse.handleEvent(event);
+}
+
+void Context::update(float dt)
+{
+    d_impl->mouse.update();
+}
+
+}
 }
--- Sprocket/UI/SimpleUI.h
+++ Sprocket/UI/SimpleUI.h
@@ -1,5 +1,24 @@
 #pragma once
+#include "Window.h"
+#include "Event.h"
+
+#include <memory>
 
 namespace Sprocket {
+namespace SimpleUI {
 
+struct SimpleUIData;
+
+class Context
+{
+    std::shared_ptr<SimpleUIData> d_impl;
+
+public:
+    Context(Window* window);
+
+    void handleEvent(Event& event);
+    void update(float dt);
+};
+
+}
 }
