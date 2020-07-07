40932daa30a463b803e66ecb758078c70a6d8462
Matt-Cummins <18335242+Matt-Cummins@users.noreply.github.com>
2020-07-07 19:00:32 +0100

    Remove opengl.h

--- Sprocket/Vendor/ftgl/opengl.h
+++
@@ -1,9 +0,0 @@
-/* Freetype GL - A C OpenGL Freetype engine
- *
- * Distributed under the OSI-approved BSD 2-Clause License.  See accompanying
- * file `LICENSE` for more details.
- */
-#ifndef __OPEN_GL_H__
-#define __OPEN_GL_H__
-
-#endif /* OPEN_GL_H */
