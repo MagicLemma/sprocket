cfd8e3f1ebe50e8ad826d8777865d87ffbf1b7bd
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-08-21 00:44:21 +0100

    Clean up FirstPersonCamera.lua

--- Resources/Scripts/FirstPersonCamera.lua
+++ Resources/Scripts/FirstPersonCamera.lua
@@ -10,24 +10,12 @@ function OnUpdate(dt)
     
     local transform = GetTransformComponent()
     local new_pos = transform.position
-    if IsKeyDown(KEYBOARD_W) then
-        new_pos = new_pos + speed * f
-    end
-    if IsKeyDown(KEYBOARD_S) then
-        new_pos = new_pos - speed * f
-    end
-    if IsKeyDown(KEYBOARD_D) then
-        new_pos = new_pos + speed * r
-    end
-    if IsKeyDown(KEYBOARD_A) then
-        new_pos = new_pos - speed * r
-    end
-    if IsKeyDown(KEYBOARD_SPACE) then
-        new_pos.y = new_pos.y + speed
-    end
-    if IsKeyDown(KEYBOARD_LSHIFT) then
-        new_pos.y = new_pos.y - speed
-    end
+    if IsKeyDown(KEYBOARD_W) then new_pos = new_pos + speed * f end
+    if IsKeyDown(KEYBOARD_S) then new_pos = new_pos - speed * f end
+    if IsKeyDown(KEYBOARD_D) then new_pos = new_pos + speed * r end
+    if IsKeyDown(KEYBOARD_A) then new_pos = new_pos - speed * r end
+    if IsKeyDown(KEYBOARD_SPACE) then new_pos.y = new_pos.y + speed end
+    if IsKeyDown(KEYBOARD_LSHIFT) then new_pos.y = new_pos.y - speed end
     transform.position = new_pos
     SetTransformComponent(transform)
 
@@ -37,7 +25,6 @@ function OnUpdate(dt)
     local camera = GetCameraComponent()
     camera.pitch = Clamp(camera.pitch - 0.15 * dy, -89, 89)
     SetCameraComponent(camera)
-    
 end
 
 function OnMouseButtonPressedEvent(consumed, button, action, mods) end
