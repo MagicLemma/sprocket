0601caf154a73bf0aae43cca46aeac39291a19cc
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-08-20 20:59:05 +0100

    Modify the current serialiser to make it easier to generate

--- Sprocket/Scene/Serialiser.cpp
+++ Sprocket/Scene/Serialiser.cpp
@@ -20,73 +20,72 @@ void Serialiser::Serialise(const std::string& file)
         out << YAML::BeginMap;
         if (entity.Has<NameComponent>()) {
             const auto& name = entity.Get<NameComponent>();
-            out << YAML::Key << "Name" << YAML::BeginMap;
-            out << YAML::Key << "Name" << YAML::Value << name.name;
+            out << YAML::Key << "NameComponent" << YAML::BeginMap;
+            out << YAML::Key << "name" << YAML::Value << name.name;
             out << YAML::EndMap;
         }
         if (entity.Has<TransformComponent>()) {
             const auto& transform = entity.Get<TransformComponent>();
-            out << YAML::Key << "Transform" << YAML::BeginMap;
-            out << YAML::Key << "Position" << YAML::Value << transform.position;
-            out << YAML::Key << "Orientation" << YAML::Value << transform.orientation;
+            out << YAML::Key << "TransformComponent" << YAML::BeginMap;
+            out << YAML::Key << "position" << YAML::Value << transform.position;
+            out << YAML::Key << "orientation" << YAML::Value << transform.orientation;
             out << YAML::EndMap;
         }
         if (entity.Has<ModelComponent>()) {
             const auto& model = entity.Get<ModelComponent>();
-            out << YAML::Key << "Model" << YAML::BeginMap;
-            out << YAML::Key << "Model" << YAML::Value << model.model;
-            out << YAML::Key << "Scale" << YAML::Value << model.scale;
-            out << YAML::Key << "Texture" << YAML::Value << model.texture;
-            out << YAML::Key << "ShineDamper" << YAML::Value << model.shineDamper;
-            out << YAML::Key << "Reflectivity" << YAML::Value << model.reflectivity;
+            out << YAML::Key << "ModelComponent" << YAML::BeginMap;
+            out << YAML::Key << "model" << YAML::Value << model.model;
+            out << YAML::Key << "scale" << YAML::Value << model.scale;
+            out << YAML::Key << "texture" << YAML::Value << model.texture;
+            out << YAML::Key << "shineDamper" << YAML::Value << model.shineDamper;
+            out << YAML::Key << "reflectivity" << YAML::Value << model.reflectivity;
             out << YAML::EndMap;
         }
         if (entity.Has<PhysicsComponent>()) {
             const auto& physics = entity.Get<PhysicsComponent>();
-            out << YAML::Key << "Physics" << YAML::BeginMap;
-            out << YAML::Key << "Velocity" << YAML::Value << physics.velocity;
-            out << YAML::Key << "Gravity" << YAML::Value << physics.gravity;
-            out << YAML::Key << "Frozen" << YAML::Value << physics.frozen;
-            out << YAML::Key << "Collider" << YAML::Value << static_cast<int>(physics.collider);
-            out << YAML::Key << "HalfExtents" << YAML::Value << physics.halfExtents;
-            out << YAML::Key << "Radius" << YAML::Value << physics.radius;
-            out << YAML::Key << "Height" << YAML::Value << physics.height;
-            out << YAML::Key << "Mass" << YAML::Value << physics.mass;
-            out << YAML::Key << "Bounciness" << YAML::Value << physics.bounciness;
-            out << YAML::Key << "FrictionCoefficient" << YAML::Value << physics.frictionCoefficient;
-            out << YAML::Key << "RollingResistance" << YAML::Value << physics.rollingResistance;
+            out << YAML::Key << "PhysicsComponents" << YAML::BeginMap;
+            out << YAML::Key << "velocity" << YAML::Value << physics.velocity;
+            out << YAML::Key << "gravity" << YAML::Value << physics.gravity;
+            out << YAML::Key << "frozen" << YAML::Value << physics.frozen;
+            out << YAML::Key << "collider" << YAML::Value << static_cast<int>(physics.collider);
+            out << YAML::Key << "halfExtents" << YAML::Value << physics.halfExtents;
+            out << YAML::Key << "radius" << YAML::Value << physics.radius;
+            out << YAML::Key << "height" << YAML::Value << physics.height;
+            out << YAML::Key << "mass" << YAML::Value << physics.mass;
+            out << YAML::Key << "bounciness" << YAML::Value << physics.bounciness;
+            out << YAML::Key << "frictionCoefficient" << YAML::Value << physics.frictionCoefficient;
+            out << YAML::Key << "rollingResistance" << YAML::Value << physics.rollingResistance;
             out << YAML::EndMap;
         }
         if (entity.Has<ScriptComponent>()) {
             const auto& script = entity.Get<ScriptComponent>();
-            out << YAML::Key << "Script" << YAML::BeginMap;
-            out << YAML::Key << "Script" << YAML::Value << script.script;
-            out << YAML::Key << "Active" << YAML::Value << script.active;
+            out << YAML::Key << "ScriptComponent" << YAML::BeginMap;
+            out << YAML::Key << "script" << YAML::Value << script.script;
+            out << YAML::Key << "active" << YAML::Value << script.active;
             out << YAML::EndMap;
         }
         if (entity.Has<CameraComponent>()) {
             const auto& camera = entity.Get<CameraComponent>();
-            out << YAML::Key << "Camera" << YAML::BeginMap;
-            out << YAML::Key << "FOV" << YAML::Value << camera.fov;
-            out << YAML::Key << "Pitch" << YAML::Value << camera.pitch;
+            out << YAML::Key << "CameraComponent" << YAML::BeginMap;
+            out << YAML::Key << "fov" << YAML::Value << camera.fov;
+            out << YAML::Key << "pitch" << YAML::Value << camera.pitch;
             out << YAML::EndMap;
         }
         if (entity.Has<SelectComponent>()) {
             const auto& select = entity.Get<SelectComponent>();
-            // No need to store the values.
-            out << YAML::Key << "Select" << true;
+            out << YAML::Key << "SelectComponent" << true;
         }
         if (entity.Has<PathComponent>()) {
             const auto& path = entity.Get<PathComponent>();
-            out << YAML::Key << "Path" << YAML::BeginMap;
-            out << YAML::Key << "Speed" << YAML::Value << path.speed;
+            out << YAML::Key << "PathComponent" << YAML::BeginMap;
+            out << YAML::Key << "speed" << YAML::Value << path.speed;
             out << YAML::EndMap;
         }
         if (entity.Has<GridComponent>()) {
             const auto& grid = entity.Get<GridComponent>();
-            out << YAML::Key << "Grid" << YAML::BeginMap;
-            out << YAML::Key << "X" << YAML::Value << grid.x;
-            out << YAML::Key << "Z" << YAML::Value << grid.z;
+            out << YAML::Key << "GridComponent" << YAML::BeginMap;
+            out << YAML::Key << "x" << YAML::Value << grid.x;
+            out << YAML::Key << "z" << YAML::Value << grid.z;
             out << YAML::EndMap;
         }
         out << YAML::EndMap;
@@ -115,76 +114,75 @@ void Serialiser::Deserialise(const std::string& file)
     for (auto entity : entities) {
         Entity e = d_scene->NewEntity();
 
-        if (auto name = entity["Name"]) {
+        if (auto name = entity["NameComponent"]) {
             NameComponent nc;
-            nc.name = name["Name"].as<std::string>();
+            nc.name = name["name"].as<std::string>();
             e.Add(nc);
         }
 
-        if (auto transform = entity["Transform"]) {
+        if (auto transform = entity["TransformComponent"]) {
             TransformComponent tc;
-            tc.position = transform["Position"].as<Maths::vec3>();
-            tc.orientation = transform["Orientation"].as<Maths::quat>();
+            tc.position = transform["position"].as<Maths::vec3>();
+            tc.orientation = transform["orientation"].as<Maths::quat>();
             e.Add(tc);
         }
 
-        if (auto model = entity["Model"]) {
+        if (auto model = entity["ModelComponent"]) {
             ModelComponent mc;
-            mc.model = model["Model"].as<std::string>();
-            mc.scale = model["Scale"].as<float>();
-            mc.texture = model["Texture"].as<std::string>();
-            mc.shineDamper = model["ShineDamper"].as<float>();
-            mc.reflectivity = model["Reflectivity"].as<float>();
+            mc.model = model["model"].as<std::string>();
+            mc.scale = model["scale"].as<float>();
+            mc.texture = model["texture"].as<std::string>();
+            mc.shineDamper = model["shineDamper"].as<float>();
+            mc.reflectivity = model["reflectivity"].as<float>();
             e.Add(mc);
         }
 
-        if (auto physics = entity["Physics"]) {
+        if (auto physics = entity["PhysicsComponent"]) {
             PhysicsComponent pc;
-            pc.velocity = physics["Velocity"].as<Maths::vec3>();
-            pc.gravity = physics["Gravity"].as<bool>();
-            pc.frozen = physics["Frozen"].as<bool>();
-            pc.collider = static_cast<Collider>(physics["Collider"].as<int>());
-            pc.halfExtents = physics["HalfExtents"].as<Maths::vec3>();
-            pc.radius = physics["Radius"].as<float>();
-            pc.height = physics["Height"].as<float>();
-            pc.mass = physics["Mass"].as<float>();
-            pc.bounciness = physics["Bounciness"].as<float>();
-            pc.frictionCoefficient = physics["FrictionCoefficient"].as<float>();
-            pc.rollingResistance = physics["RollingResistance"].as<float>();
+            pc.velocity = physics["velocity"].as<Maths::vec3>();
+            pc.gravity = physics["gravity"].as<bool>();
+            pc.frozen = physics["frozen"].as<bool>();
+            pc.collider = static_cast<Collider>(physics["collider"].as<int>());
+            pc.halfExtents = physics["halfExtents"].as<Maths::vec3>();
+            pc.radius = physics["radius"].as<float>();
+            pc.height = physics["height"].as<float>();
+            pc.mass = physics["mass"].as<float>();
+            pc.bounciness = physics["bounciness"].as<float>();
+            pc.frictionCoefficient = physics["frictionCoefficient"].as<float>();
+            pc.rollingResistance = physics["rollingResistance"].as<float>();
             e.Add(pc);
         }
 
-        if (auto camera = entity["Camera"]) {
+        if (auto camera = entity["CameraComponent"]) {
             CameraComponent cc;
             // Projection does not need to be stored
-            cc.pitch = camera["FOV"].as<float>();
-            cc.pitch = camera["Pitch"].as<float>();
+            cc.pitch = camera["fov"].as<float>();
+            cc.pitch = camera["pitch"].as<float>();
             e.Add(cc);
         }
 
-        if (auto select = entity["Select"]) {
+        if (auto select = entity["SelectComponent"]) {
             SelectComponent sc;
             e.Add(sc);
         }
 
-        if (auto path = entity["Path"]) {
+        if (auto path = entity["PathComponent"]) {
             PathComponent pc;
-            pc.speed = path["Speed"].as<float>();
+            pc.speed = path["speed"].as<float>();
             e.Add(pc);
         }
 
-        if (auto grid = entity["Grid"]) {
+        if (auto grid = entity["GridComponent"]) {
             GridComponent gc;
-            gc.x = grid["X"].as<int>();
-            gc.z = grid["Z"].as<int>();
+            gc.x = grid["x"].as<int>();
+            gc.z = grid["z"].as<int>();
             e.Add(gc);
         }
 
-        // This needs to get added last as scripts may access other components.
-        if (auto script = entity["Script"]) {
+        if (auto script = entity["ScriptComponent"]) {
             ScriptComponent sc;
-            sc.script = script["Script"].as<std::string>();
-            sc.active = script["Active"].as<bool>();
+            sc.script = script["script"].as<std::string>();
+            sc.active = script["active"].as<bool>();
             e.Add(sc);
         }
     }
