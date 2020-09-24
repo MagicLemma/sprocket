7698248bd549da0d3ed71442a3358e7d940d48f2
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-09-25 00:22:48 +0100

    Add Mesh files

---
+++ Sprocket/Graphics/Mesh.cpp
@@ -0,0 +1,61 @@
+#include "Mesh.h"
+
+#include <cassert>
+#include <glad/glad.h>
+
+namespace Sprocket {
+
+Mesh::Mesh(const Vertex3DBuffer& vertices, const IndexBuffer& indices)
+    : d_vertexBuffer(std::make_shared<VBO>())
+    , d_indexBuffer(std::make_shared<VBO>())
+    , d_count(indices.size())
+    , d_layout(sizeof(Vertex3D), 0)
+{
+    glBindBuffer(GL_ARRAY_BUFFER, d_vertexBuffer->Value());
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex3D) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, d_indexBuffer->Value());
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 2);
+    assert(d_layout.Validate());
+}
+
+Mesh::Mesh()
+    : d_layout(sizeof(Vertex3D), 0)
+{
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 3);
+    d_layout.AddAttribute(DataType::FLOAT, 2);
+    assert(d_layout.Validate());
+}
+
+void Mesh::Bind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, d_vertexBuffer->Value());
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, d_indexBuffer->Value());
+    d_layout.SetAttributes();
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+bool Mesh::operator==(const Mesh& other) const
+{
+    // Two models are the same if they point to the same VAO.
+    return d_vertexBuffer->Value() == other.d_vertexBuffer->Value() &&
+           d_indexBuffer->Value() == other.d_indexBuffer->Value();
+}
+
+BufferLayout Mesh::GetLayout() const
+{
+    return d_layout;
+}
+
+}
