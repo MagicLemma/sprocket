b6fea8ed7e39572813c6d4129238e1d598857e12
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-11-10 21:37:24 +0000

    Remove much of the editor from runtime

--- Runtime/EditorLayer.cpp
+++ Runtime/EditorLayer.cpp
@@ -113,24 +113,20 @@ void EditorLayer::OnUpdate(double dt)
 
     d_entityRenderer.EnableParticles(&d_particleManager);
 
-    d_viewport.Bind();
     if (d_playingGame) {
-        d_entityRenderer.Draw(d_runtimeCamera, *d_activeScene);
         d_skyboxRenderer.Draw(d_skybox, d_runtimeCamera);
+        d_entityRenderer.Draw(d_runtimeCamera, *d_activeScene);
         if (d_showColliders) {
             d_colliderRenderer.Draw(d_runtimeCamera, *d_activeScene);
         }
     }
     else {
-        d_entityRenderer.Draw(d_editorCamera.Proj(), d_editorCamera.View(), *d_activeScene);
         d_skyboxRenderer.Draw(d_skybox, d_editorCamera.Proj(), d_editorCamera.View());
+        d_entityRenderer.Draw(d_editorCamera.Proj(), d_editorCamera.View(), *d_activeScene);
         if (d_showColliders) {
             d_colliderRenderer.Draw(d_editorCamera.Proj(), d_editorCamera.View(), *d_activeScene);
         }
     }
-
-
-    d_viewport.Unbind();
 
     d_ui.StartFrame();
 
@@ -152,41 +148,6 @@ void EditorLayer::OnUpdate(double dt)
     float menuBarHeight = 19.0f;
 
     if (ImGui::BeginMainMenuBar()) {
-        if (ImGui::BeginMenu("File")) {
-            if (ImGui::MenuItem("New")) {
-                std::string file = SaveFile(d_core.window, "*.yaml");
-                if (!file.empty()) {
-                    SPKT_LOG_INFO("Creating {}...", d_sceneFile);
-                    d_sceneFile = file;
-                    d_scene->Clear();
-                    SPKT_LOG_INFO("...done!");
-                }
-            }
-            if (ImGui::MenuItem("Open")) {
-                std::string file = OpenFile(d_core.window, "*.yaml");
-                if (!file.empty()) {
-                    SPKT_LOG_INFO("Loading {}...", d_sceneFile);
-                    d_sceneFile = file;
-                    Loader::Load(file, d_scene);
-                    SPKT_LOG_INFO("...done!");
-                }
-            }
-            if (ImGui::MenuItem("Save")) {
-                SPKT_LOG_INFO("Saving {}...", d_sceneFile);
-                Loader::Save(d_sceneFile, d_scene);
-                SPKT_LOG_INFO("...done!");
-            }
-            if (ImGui::MenuItem("Save As")) {
-                std::string file = SaveFile(d_core.window, "*.yaml");
-                if (!file.empty()) {
-                    SPKT_LOG_INFO("Saving as {}...", file);
-                    d_sceneFile = file;
-                    Loader::Save(file, d_scene);
-                    SPKT_LOG_INFO("...done!");
-                }
-            }
-            ImGui::EndMenu();
-        }
         if (ImGui::BeginMenu("Scene")) {
             if (ImGui::MenuItem("Run")) {
                 auto runningScene = std::make_shared<Scene>();
@@ -210,133 +171,6 @@ void EditorLayer::OnUpdate(double dt)
             ImGui::EndMenu();
         }
         ImGui::EndMainMenuBar();
-    }
-
-    float w = (float)d_core.window->Width();
-    float h = (float)d_core.window->Height();
-
-    // VIEWPORT
-    ImGui::SetNextWindowPos({0.0, menuBarHeight});
-    ImGui::SetNextWindowSize({0.8f * w, 0.8f * h});
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, {0, 0});
-    if (ImGui::Begin("Viewport", &open, flags | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse)) {
-        d_isViewportHovered = ImGui::IsWindowHovered();
-        d_isViewportFocused = ImGui::IsWindowFocused();
-        d_ui.BlockEvents(!d_isViewportFocused || !d_isViewportHovered);
-        ImGuiXtra::Image(d_viewport.GetColour(), {0.8f * w, 0.8f * h});
-        ImGuiXtra::SetGuizmo();
-        ImGui::End();
-    }
-    ImGui::PopStyleVar();
-
-    // INSPECTOR
-    ImGui::SetNextWindowPos({0.8f * w, menuBarHeight + (h - menuBarHeight)/2.0f});
-    ImGui::SetNextWindowSize({0.2f * w, (h - menuBarHeight)/2.0f});
-    if (ImGui::Begin("Inspector", &open, flags)) {
-        d_inspector.Show(*this);
-        ImGui::End();
-    }
-
-    // BOTTOM PANEL
-    ImGui::SetNextWindowPos({0.0, 0.8f * h + menuBarHeight});
-    ImGui::SetNextWindowSize({0.8f * w, h - menuBarHeight - 0.8f * h});
-    if (ImGui::Begin("BottomPanel", &open, flags)) {
-        ImGui::Checkbox("Show Colliders", &d_showColliders);
-        ImGui::Text(fmt::format("Loading Meshes: {}", d_core.assetManager->IsLoadingMeshes() ? "Yes" : "No").c_str());
-        ImGui::Text(fmt::format("Loading Textures: {}", d_core.assetManager->IsLoadingTextures() ? "Yes" : "No").c_str());
-        ImGui::End();
-    }
-
-    // EXPLORER
-    ImGui::SetNextWindowPos({0.8f * w, menuBarHeight});
-    ImGui::SetNextWindowSize({0.2f * w, (h - menuBarHeight)/2.0f});
-    static std::string search;
-    if (ImGui::Begin("Explorer", &open, flags | ImGuiWindowFlags_NoDecoration)) {
-        ImGuiXtra::TextModifiable(search);
-        ImGui::SameLine();
-        if (ImGui::Button("X")) {
-            search = "";
-        }
-        if (ImGui::BeginTabBar("##Tabs")) {
-            
-            if (ImGui::BeginTabItem("Entities")) {
-                ImGui::BeginChild("Entity List");
-                d_scene->All([&](Entity& entity) {
-                    if (SubstringCI(Name(entity), search)) {
-                        ImGui::PushID(entity.Id());
-                        if (ImGui::Selectable(Name(entity).c_str())) {
-                            d_selected = entity;
-                        }
-                        ImGui::PopID();
-                    }
-                });
-                ImGui::EndChild();
-                ImGui::EndTabItem();
-            }
-
-            if (ImGui::BeginTabItem("Materials")) {
-                ImGui::BeginChild("Material List");
-                for (auto& [file, material] : d_core.assetManager->Materials()) {
-                    ImGui::PushID(std::hash<std::string>{}(material->file));
-                    if (ImGui::CollapsingHeader(material->name.c_str())) {
-                        ImGui::Text(file.c_str());
-                        ImGui::Separator();
-
-                        ImGui::PushID(std::hash<std::string>{}("Albedo"));
-                        ImGui::Text("Albedo");
-                        ImGui::Checkbox("Use Map", &material->useAlbedoMap);
-                        if (material->useAlbedoMap) {
-                            MaterialUI(material->albedoMap);
-                        } else {
-                            ImGui::ColorEdit3("##Albedo", &material->albedo.x);
-                        }
-                        ImGui::PopID();
-                        ImGui::Separator();
-
-                        ImGui::PushID(std::hash<std::string>{}("Normal"));
-                        ImGui::Text("Normal");
-                        ImGui::Checkbox("Use Map", &material->useNormalMap);
-                        if (material->useNormalMap) {
-                            MaterialUI(material->normalMap);
-                        }
-                        ImGui::PopID();
-                        ImGui::Separator();
-
-                        ImGui::PushID(std::hash<std::string>{}("Metallic"));
-                        ImGui::Text("Metallic");
-                        ImGui::Checkbox("Use Map", &material->useMetallicMap);
-                        if (material->useMetallicMap) {
-                            MaterialUI(material->metallicMap);
-                        } else {
-                            ImGui::DragFloat("##Metallic", &material->metallic, 0.01f, 0.0f, 1.0f);
-                        }
-                        ImGui::PopID();
-                        ImGui::Separator();
-                        
-                        ImGui::PushID(std::hash<std::string>{}("Roughness"));
-                        ImGui::Text("Roughness");
-                        ImGui::Checkbox("Use Map", &material->useRoughnessMap);
-                        if (material->useRoughnessMap) {
-                            MaterialUI(material->roughnessMap);
-                        } else {
-                            ImGui::DragFloat("##Roughness", &material->roughness, 0.01f, 0.0f, 1.0f);
-                        }
-                        ImGui::PopID();
-                        ImGui::Separator();
-
-                        if (ImGui::Button("Save")) {
-                            material->Save();
-                        }
-                    }
-                    ImGui::PopID();
-                }
-                ImGui::EndChild();
-                ImGui::EndTabItem();
-            }
-
-            ImGui::EndTabBar();
-        }
-        ImGui::End();
     }
 
     d_ui.EndFrame();    
