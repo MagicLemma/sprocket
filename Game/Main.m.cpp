b453702b2adb4c83585777a7bdbdf11da00c3334
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-06 03:17:24 +0000

    Add Sorting to ECS

--- Game/Main.m.cpp
+++ Game/Main.m.cpp
@@ -1,14 +1,6 @@
 #include <Sprocket.h>
 #include "Game.h"
 
-struct Foo {
-    int x = 5;
-    Foo(int y) : x(y) {}
-    ~Foo() {
-        SPKT_LOG_INFO("Deleting just fine!");
-    }
-};
-
 int main()
 {
     //Sprocket::Window window("Game");
@@ -21,10 +13,21 @@ int main()
     Log::Init();
 
     ECS::Registry reg;
-
-    ECS::Entity e1 = reg.New();
-    reg.Delete(e1);
-    SPKT_LOG_INFO("Valid? {}", e1.Valid());
-
-    ECS::Entity e2 = reg.New();
+    u64 e1 = reg.New();
+    u64 e2 = reg.New();
+    u64 e3 = reg.New();
+    u64 e6 = reg.New();
+    u64 e4 = reg.New();
+    u64 e5 = reg.New();
+    reg.Add(e1, NameComponent({"E"}));
+    reg.Add(e2, NameComponent({"D"}));
+    reg.Add(e3, NameComponent({"C"}));
+    reg.Add(e4, NameComponent({"B"}));
+    reg.Add(e5, NameComponent({"A"}));
+    reg.Sort<NameComponent>([&](u64 a, u64 b) {
+        auto an = reg.Get<NameComponent>(a).name;
+        auto bn = reg.Get<NameComponent>(b).name;
+        bool x = an < bn;
+        return x;
+    });
 }
