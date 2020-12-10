dc631bd9d273515ef93c49ef1ccb23e9b723682b
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-10 01:10:47 +0000

    Reenable Game logic

--- Game/Main.m.cpp
+++ Game/Main.m.cpp
@@ -3,25 +3,9 @@
 
 int main()
 {
-    //Sprocket::Window window("Game");
-    //WorldLayer game(&window);
-    //Sprocket::RunOptions options;
-    //options.showFramerate = true;
-    //return Sprocket::Run(game, window, options);
-
-    using namespace Sprocket;
-    Log::Init();
-
-    ECS::Registry reg;
-    ECS::Entity e1 = reg.New();
-    e1.Add<NameComponent>({"A"});
-
-    ECS::Entity e2 = reg.New();
-    e2.Add<NameComponent>({"D"});
-
-    ECS::Entity e3 = reg.New();
-    e3.Add<NameComponent>({"B"});
-
-    ECS::Entity e4 = reg.New();
-    e4.Add<NameComponent>({"C"});
+    Sprocket::Window window("Game");
+    WorldLayer game(&window);
+    Sprocket::RunOptions options;
+    options.showFramerate = true;
+    return Sprocket::Run(game, window, options);
 }
