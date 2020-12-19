959b289360ff4d99eb6f9115d87cbc216fc5897e
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-19 01:20:03 +0000

    Move the comments in Camera.h

--- Sprocket/Scene/Camera.h
+++ Sprocket/Scene/Camera.h
@@ -4,14 +4,14 @@
 
 namespace Sprocket {
 
+// Returns the view matrix from the entity's position.
+// If the entity has a CameraComponent, the pitch is also
+// taken into account.
 glm::mat4 MakeView(const ECS::Entity& camera);
-    // Returns the view matrix from the entity's position.
-    // If the entity has a CameraComponent, the pitch is also
-    // taken into account.
 
+// Returns the projection matrix from the entity's
+// CameraComponent. If the entity does not have the component,
+// an identity matrix is returned.
 glm::mat4 MakeProj(const ECS::Entity& camera);
-    // Returns the projection matrix from the entity's
-    // CameraComponent. If the entity does not have the component,
-    // an identity matrix is returned.
     
 }
