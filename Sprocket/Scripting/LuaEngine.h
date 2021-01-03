706b1422a6e1896b9317a490225bde65b9d56eb4
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2021-01-03 01:55:26 +0000

    Add a more generic Call function

--- Sprocket/Scripting/LuaEngine.h
+++ Sprocket/Scripting/LuaEngine.h
@@ -25,11 +25,17 @@ class LuaEngine
     template <typename T>
     void Push(T* val);
 
+    template <typename Arg, typename... Args>
+    void PushValues(Arg&& arg, Args&&... args);
+
 public:
     LuaEngine();
     ~LuaEngine();
 
     void RunScript(const std::string& filename);
+
+    template <typename... Args>
+    void Call(const std::string& function, Args&&... args);
 
     void CallInitFunction(ecs::Entity entity);
     void CallOnUpdateFunction(ecs::Entity entity, double dt);
@@ -90,6 +96,28 @@ template <typename T>
 void LuaEngine::Push(T* val)
 {
     lua_pushlightuserdata(d_L, (void*)val);
+}
+
+template <typename Arg, typename... Args>
+void PushValues(Arg&& arg, Args&&... args)
+{
+    Push(std::forward<Arg>(arg));
+    Push(std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+void Call(const std::string& function, Args&&... args)
+{
+    lua_getglobal(d_L, function.c_str());
+    if (!lua_isfunction(d_L, -1)) {
+        lua_pop(d_L, -1);
+        return;
+    }
+
+    PushValues(std::forward<Args>(args)...);
+
+    int rc = lua_pcall(d_L, sizeof...(Args), 0, 0);
+    PrintErrors(d_L, rc);
 }
 
 }
