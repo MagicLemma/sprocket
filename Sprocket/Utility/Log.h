ba439ad0793355c791e4c3b432a769501177a3f9
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2021-03-02 21:00:37 +0000

    Move implementation of the logging functions to the cpp file, and add timestamp

--- Sprocket/Utility/Log.h
+++ Sprocket/Utility/Log.h
@@ -7,45 +7,41 @@
 namespace Sprocket {
 namespace log {
 
-constexpr fmt::color BASE = fmt::color::ghost_white;
-
 void init();
 
-template <typename Format, typename... Args>
-void warn(Format&& format, Args&&... args)
+template <typename... Args>
+void warn(std::string_view format, Args&&... args)
 {
-    std::string message = fmt::format(format, std::forward<Args>(args)...);
-    fmt::print(fmt::fg(BASE), "[");
-    fmt::print(fmt::fg(fmt::color::aqua), "WARN");
-    fmt::print(fmt::fg(BASE), "]: {}\n", message);
+    std::string prefix = fmt::format(fmt::fg(fmt::color::aqua), "WARN");
+    std::string log = fmt::format(format, std::forward<Args>(args)...);
+    prefixed_log(prefix, log);
 }
 
-template <typename Format, typename... Args>
-void info(Format&& format, Args&&... args)
+template <typename... Args>
+void info(std::string_view format, Args&&... args)
 {
-    std::string message = fmt::format(format, std::forward<Args>(args)...);
-    fmt::print(fmt::fg(BASE), "[");
-    fmt::print(fmt::fg(fmt::color::light_green), "INFO");
-    fmt::print(fmt::fg(BASE), "]: {}\n", message);
+    std::string prefix = fmt::format(fmt::fg(fmt::color::light_green), "INFO");
+    std::string log = fmt::format(format, std::forward<Args>(args)...);
+    prefixed_log(prefix, log);
 }
 
-template <typename Format, typename... Args>
-void error(Format&& format, Args&&... args)
+template <typename... Args>
+void error(std::string_view format, Args&&... args)
 {
-    std::string message = fmt::format(format, std::forward<Args>(args)...);
-    fmt::print(fmt::fg(BASE), "[");
-    fmt::print(fmt::fg(fmt::color::orange), "ERROR");
-    fmt::print(fmt::fg(BASE), "]: {}\n", message);
+    std::string prefix = fmt::format(fmt::fg(fmt::color::orange), "ERROR");
+    std::string log = fmt::format(format, std::forward<Args>(args)...);
+    prefixed_log(prefix, log);
 }
 
-template <typename Format, typename... Args>
-void fatal(Format& format, Args&&... args)
+template <typename... Args>
+void fatal(std::string_view format, Args&&... args)
 {
-    std::string message = fmt::format(format, std::forward<Args>(args)...);
-    fmt::print(fmt::fg(BASE), "[");
-    fmt::print(fmt::fg(fmt::color::magenta), "FATAL");
-    fmt::print(fmt::fg(BASE), "]: {}\n", message);
+    std::string prefix = fmt::format(fmt::fg(fmt::color::magenta), "FATAL");
+    std::string log = fmt::format(format, std::forward<Args>(args)...);
+    prefixed_log(prefix, log);
 }
+
+void prefixed_log(std::string_view prefix, std::string_view message);
 
 }
 }
