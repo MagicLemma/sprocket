414f4215fbe1953e70f2e11c5674f2992a40b654
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2021-03-02 01:06:03 +0000

    Remove spdlog

--- Sprocket/Utility/Log.h
+++ Sprocket/Utility/Log.h
@@ -9,32 +9,33 @@ namespace log {
 
 void init();
 
-template <typename... Args>
-void warn(const char* format, Args&&... args)
+template <typename Format, typename... Args>
+void warn(Format&& format, Args&&... args)
 {
     std::string message = fmt::format(format, std::forward<Args>(args)...);
     fmt::print(fmt::fg(fmt::color::azure), "[WARN]: {}\n", message);
 }
 
-template <typename... Args>
-void info(const char* format, Args&&... args)
+template <typename Format, typename... Args>
+void info(Format&& format, Args&&... args)
 {
     std::string message = fmt::format(format, std::forward<Args>(args)...);
     fmt::print(fmt::fg(fmt::color::light_green), "[INFO]: {}\n", message);
 }
 
-template <typename... Args>
-void error(const char* format, Args&&... args)
+template <typename Format, typename... Args>
+void error(Format&& format, Args&&... args)
 {
     std::string message = fmt::format(format, std::forward<Args>(args)...);
     fmt::print(fmt::fg(fmt::color::orange), "[ERROR]: {}\n", message);
 }
 
-template <typename... Args>
-void fatal(const char* format, Args&&... args)
+template <typename Format, typename... Args>
+void fatal(Format& format, Args&&... args)
 {
     std::string message = fmt::format(format, std::forward<Args>(args)...);
     fmt::print(fmt::fg(fmt::color::magenta), "[FATAL]: {}\n", message);
 }
+
 }
 }
