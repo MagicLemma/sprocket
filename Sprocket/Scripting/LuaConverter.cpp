bf7ac8cf688370d15919b5a19d5bba3ef59440b7
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2021-03-08 18:49:34 +0000

    Remove Pull functions from generated library code

--- Sprocket/Scripting/LuaConverter.cpp
+++ Sprocket/Scripting/LuaConverter.cpp
@@ -21,6 +21,11 @@ int Converter<int>::push_to(lua_State* L, const int& value)
     return 1;
 }
 
+int Converter<int>::get_from_signature(lua_State* L, int& count)
+{
+    return static_cast<int>(lua_tointeger(L, count++));
+}
+
 
 u32 Converter<u32>::pull_from(lua_State* L)
 {
@@ -34,6 +39,11 @@ int Converter<u32>::push_to(lua_State* L, const u32& value)
 {
     lua_pushinteger(L, value);
     return 1;
+}
+
+u32 Converter<u32>::get_from_signature(lua_State* L, int& count)
+{
+    return static_cast<u32>(lua_tointeger(L, count++));
 }
 
 
@@ -51,6 +61,11 @@ int Converter<bool>::push_to(lua_State* L, const bool& value)
     return 1;
 }
 
+bool Converter<bool>::get_from_signature(lua_State* L, int& count)
+{
+    return static_cast<bool>(lua_toboolean(L, count++));
+}
+
 
 double Converter<double>::pull_from(lua_State* L)
 {
@@ -64,6 +79,11 @@ int Converter<double>::push_to(lua_State* L, const double& value)
 {
     lua_pushnumber(L, value);
     return 1;
+}
+
+double Converter<double>::get_from_signature(lua_State* L, int& count)
+{
+    return static_cast<double>(lua_tonumber(L, count++));
 }
 
 
@@ -81,6 +101,11 @@ int Converter<float>::push_to(lua_State* L, const float& value)
     return 1;
 }
 
+float Converter<float>::get_from_signature(lua_State* L, int& count)
+{
+    return static_cast<float>(lua_tonumber(L, count++));
+}
+
 
 const char* Converter<const char*>::pull_from(lua_State* L)
 {
@@ -94,6 +119,11 @@ int Converter<const char*>::push_to(lua_State* L, const char* value)
 {
     lua_pushstring(L, value);
     return 1;
+}
+
+const char* Converter<const char*>::get_from_signature(lua_State* L, int& count)
+{
+    return lua_tostring(L, count++);
 }
 
 
@@ -111,10 +141,15 @@ int Converter<std::string>::push_to(lua_State* L, const std::string& value)
     return 1;
 }
 
+std::string Converter<std::string>::get_from_signature(lua_State* L, int& count)
+{
+    return std::string(lua_tostring(L, count++));
+}
+
 
 void* Converter<void*>::pull_from(lua_State* L)
 {
-    assert(lua_islightuserdata(L, -1) || lua_isuserdata(L, -1));
+    assert(lua_islightuserdata(L, -1));
     auto val = lua_touserdata(L, -1);
     lua_pop(L, 1);
     return val;
@@ -124,6 +159,11 @@ int Converter<void*>::push_to(lua_State* L, void* value)
 {
     lua_pushlightuserdata(L, value);
     return 1;
+}
+
+void* Converter<void*>::get_from_signature(lua_State* L, int& count)
+{
+    return lua_touserdata(L, count++);
 }
 
 
@@ -142,12 +182,24 @@ int Converter<ecs::Entity>::push_to(lua_State* L, ecs::Entity value)
     return 1;
 }
 
+ecs::Entity Converter<ecs::Entity>::get_from_signature(lua_State* L, int& count)
+{
+    return *static_cast<ecs::Entity*>(lua_touserdata(L, count++));
+}
+
 
 int Converter<glm::vec2>::push_to(lua_State* L, const glm::vec2& value)
 {
     Converter<float>::push_to(L, value.x);
     Converter<float>::push_to(L, value.y);
     return 2;
+}
+
+glm::vec2 Converter<glm::vec2>::get_from_signature(lua_State* L, int& count)
+{
+    auto x = (float)lua_tonumber(L, count++);
+    auto y = (float)lua_tonumber(L, count++);
+    return {x, y};
 }
 
 
@@ -157,6 +209,14 @@ int Converter<glm::vec3>::push_to(lua_State* L, const glm::vec3& value)
     Converter<float>::push_to(L, value.y);
     Converter<float>::push_to(L, value.z);
     return 3;
+}
+
+glm::vec3 Converter<glm::vec3>::get_from_signature(lua_State* L, int& count)
+{
+    auto x = (float)lua_tonumber(L, count++);
+    auto y = (float)lua_tonumber(L, count++);
+    auto z = (float)lua_tonumber(L, count++);
+    return {x, y, z};
 }
 
 
--- Sprocket/Scripting/LuaConverter.h
+++ Sprocket/Scripting/LuaConverter.h
@@ -14,61 +14,76 @@ namespace lua {
 
 template <typename T> struct Converter
 {
-    static T pull_from(lua_State* L) {
-        static_assert(false);
-    }
+    // Pulls from the top of the stack and removes the values.
+    static T pull_from(lua_State* L) { static_assert(false); }
 
-    static int push_to(lua_State* L, const T& value) {
-        static_assert(false);
-    }
+    // Pushes the given value to the top of the stack. More complicated object are pushed
+    // as their parts, for example, vec3 is pushed as 3 floats.
+    static int push_to(lua_State* L, const T& value) { static_assert(false); }
+
+    // Constructs a T by getting values from the stack starting at position "count",
+    // and goes up the stack if it needs more than one value. "count" will be modified
+    // to point at the next position in the stack immediately after the last element
+    // used to make T. No values are popped from the stack. This is intended to be used
+    // in C++ implementations of Lua functions where more complicated objects must be
+    // passed by their parts, such as a vec3 being passed as 3 floats.
+    static T get_from_signature(lua_State* L, int& count) { static_assert(false); }
 };
 
 template <> struct Converter<int>
 {
     static int pull_from(lua_State* L);
     static int push_to(lua_State* L, const int& value);
+    static int get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<u32>
 {
     static u32 pull_from(lua_State* L);
     static int push_to(lua_State* L, const u32& value);
+    static u32 get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<bool>
 {
     static bool pull_from(lua_State* L);
     static int push_to(lua_State* L, const bool& value);
+    static bool get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<float>
 {
     static float pull_from(lua_State* L);
     static int push_to(lua_State* L, const float& value);
+    static float get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<double>
 {
     static double pull_from(lua_State* L);
     static int push_to(lua_State* L, const double& value);
+    static double get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<const char*>
 {
     static const char* pull_from(lua_State* L);
     static int push_to(lua_State* L, const char* value);
+    static const char* get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<std::string>
 {
     static std::string pull_from(lua_State* L);
     static int push_to(lua_State* L, const std::string& value);
+    static std::string get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<void*>
 {
     static void* pull_from(lua_State* L);
     static int push_to(lua_State* L, void* value);
+    static void* get_from_signature(lua_State* L, int& count);
 };
 
 template <typename T> struct Converter<T*>
@@ -80,12 +95,17 @@ template <typename T> struct Converter<T*>
     static int push_to(lua_State* L, T* value) {
         return Converter<void*>::push_to(L, static_cast<void*>(value));
     }
+
+    static T* get_from_signature(lua_State* L, int& count) {
+        return static_cast<T*>(Converter<void*>::get_from_signature(L, count));
+    }
 };
 
 template <> struct Converter<ecs::Entity>
 {
     static ecs::Entity pull_from(lua_State* L);
     static int push_to(lua_State* L, ecs::Entity value);
+    static ecs::Entity get_from_signature(lua_State* L, int& count);
 };
 
 // Implement the pull_from for these types when we need them, otherwise we are
@@ -95,12 +115,14 @@ template <> struct Converter<glm::vec2>
 {
     static glm::vec2 pull_from(lua_State* L) { return glm::vec2{1.0}; } // TODO: Implement
     static int push_to(lua_State* L, const glm::vec2& value);
+    static glm::vec2 get_from_signature(lua_State* L, int& count);
 };
 
 template <> struct Converter<glm::vec3>
 {
     static glm::vec3 pull_from(lua_State* L) { return glm::vec3{1.0}; } // TODO: Implement
     static int push_to(lua_State* L, const glm::vec3& value);
+    static glm::vec3 get_from_signature(lua_State* L, int& count);
 };
 
 }
