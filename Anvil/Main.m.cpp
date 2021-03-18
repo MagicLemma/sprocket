0f6b68e0c754c0d14814fe789ae6fafeea218c9c
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2021-03-18 18:14:21 +0000

    Remove test idenfitier enum class from main

--- Anvil/Main.m.cpp
+++ Anvil/Main.m.cpp
@@ -1,11 +1,6 @@
 #include <Sprocket.h>
 #include "Anvil.h"
 
-
-// See https://gist.github.com/zester/2438462 for better Lua integration
-
-enum class Foo : std::uint64_t {};
-
 int main()
 {
     Sprocket::Window window("Anvil");
