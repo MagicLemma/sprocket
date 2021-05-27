baef26e60386b45f0cd8a69144133779a336243a
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-05-27 21:22:36 +0100

    Reflect datamatic change of using SingleDispatch class

--- Sprocket/Utility/Maths.dmx.py
+++ Sprocket/Utility/Maths.dmx.py
@@ -1,9 +1,9 @@
 """
 A module for loading in the Maths types to Datamatic.
 """
-from Datamatic.Types import register, parse
+from Datamatic.Types import parse
 
-@register("glm::vec2")
+@parse.register("glm::vec2")
 def _(typename, obj) -> str:
     assert isinstance(obj, list)
     assert len(obj) == 2
@@ -11,7 +11,7 @@ def _(typename, obj) -> str:
     return f"{typename}{{{rep}}}"
 
 
-@register("glm::vec3")
+@parse.register("glm::vec3")
 def _(typename, obj) -> str:
     assert isinstance(obj, list)
     assert len(obj) == 3
@@ -19,8 +19,8 @@ def _(typename, obj) -> str:
     return f"{typename}{{{rep}}}"
 
 
-@register("glm::vec4")
-@register("glm::quat")
+@parse.register("glm::vec4")
+@parse.register("glm::quat")
 def _(typename, obj) -> str:
     assert isinstance(obj, list)
     assert len(obj) == 4
@@ -28,17 +28,17 @@ def _(typename, obj) -> str:
     return f"{typename}{{{rep}}}"
 
 
-@register("ecs::Identifier")
+@parse.register("ecs::Identifier")
 def _(typename, obj) -> str:
     assert isinstance(obj, int)
     return f'static_cast<{typename}>({obj})'
 
 
-@register("glm::mat4") # TODO: Implement
+@parse.register("glm::mat4") # TODO: Implement
 def _(typename, obj) -> str:
     return f"{typename}{{1.0}}"
 
 
-@register("std::queue<glm::vec3>") # TODO: Implement
+@parse.register("std::queue<glm::vec3>") # TODO: Implement
 def _(typename, obj) -> str:
     return f"{typename}{{}}"
