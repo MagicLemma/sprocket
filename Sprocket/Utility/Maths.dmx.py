ab5c64660aa584aa79f338f4d9546ea1498931de
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-05-28 02:23:20 +0100

    Use Datamatic Parametrised and Templatised Type Parsers (#73)
    * Combine all `glm` parser definitions into one via a parametrised parser.
    * Remove the parser for `std::queue<glm::vec3>` since datamatic now has a templatised parser for `std::queue<T>` so only a parser for `glm::vec3` is needed.

--- Sprocket/Utility/Maths.dmx.py
+++ Sprocket/Utility/Maths.dmx.py
@@ -3,27 +3,14 @@ A module for loading in the Maths types to Datamatic.
 """
 from Datamatic.Types import parse
 
-@parse.register("glm::vec2")
-def _(typename, obj) -> str:
+
+@parse.register("glm::vec2", length=2)
+@parse.register("glm::vec3", length=3)
+@parse.register("glm::vec4", length=4)
+@parse.register("glm::quat", length=4)
+def _(typename, obj, length) -> str:
     assert isinstance(obj, list)
-    assert len(obj) == 2
-    rep = ", ".join(parse("float", val) for val in obj)
-    return f"{typename}{{{rep}}}"
-
-
-@parse.register("glm::vec3")
-def _(typename, obj) -> str:
-    assert isinstance(obj, list)
-    assert len(obj) == 3
-    rep = ", ".join(parse("float", val) for val in obj)
-    return f"{typename}{{{rep}}}"
-
-
-@parse.register("glm::vec4")
-@parse.register("glm::quat")
-def _(typename, obj) -> str:
-    assert isinstance(obj, list)
-    assert len(obj) == 4
+    assert len(obj) == length
     rep = ", ".join(parse("float", val) for val in obj)
     return f"{typename}{{{rep}}}"
 
@@ -37,8 +24,3 @@ def _(typename, obj) -> str:
 @parse.register("glm::mat4") # TODO: Implement
 def _(typename, obj) -> str:
     return f"{typename}{{1.0}}"
-
-
-@parse.register("std::queue<glm::vec3>") # TODO: Implement
-def _(typename, obj) -> str:
-    return f"{typename}{{}}"
