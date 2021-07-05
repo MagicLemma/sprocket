cf547e7c3256771a08aae658559f00638c4a4385
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-07-05 20:19:18 +0100

    Clean up the particle systen a bit

--- Sprocket/Scene/Systems/ParticleSystem.cpp
+++ Sprocket/Scene/Systems/ParticleSystem.cpp
@@ -7,12 +7,6 @@
 namespace spkt {
 namespace {
 
-void emit_particle(ParticleSingleton& ps, const particle& p)
-{
-    (*ps.particles)[ps.next_slot] = p;
-    ps.next_slot = --ps.next_slot % NUM_PARTICLES;
-}
-
 ParticleSingleton& get_particle_runtime(apx::registry& registry)
 {
     auto entity = registry.find<ParticleSingleton>();
@@ -26,38 +20,49 @@ ParticleSingleton& get_particle_runtime(apx::registry& registry)
     return registry.get<ParticleSingleton>(entity);
 }
 
+glm::vec3 random_noise(float R)
+// Generates a random point in a sphere of radius R
+{
+    float r = Random<float>(0, R);
+    float phi = Random<float>(0, 3.142f);
+    float theta = Random<float>(0, 6.284f);
+    return {
+        r * glm::sin(theta) * glm::cos(phi),
+        r * glm::sin(theta) * glm::sin(phi),
+        r * glm::cos(theta)
+    };
+}
+
 }
 
 void particle_system(apx::registry& registry, double dt)
 {
     auto& ps = get_particle_runtime(registry);
 
+    // Update the positions of all existing particles
     for (auto& particle : *ps.particles) {
         particle.life -= dt;
         particle.position += particle.velocity * (float)dt;
         particle.velocity += particle.acceleration * (float)dt;
     }
 
+    // Go through all particle emitter entities and check to see if they need to emit
+    // any particles this frame. If they do, add them to the particle array.
     for (auto entity : registry.view<ParticleComponent>()) {
         auto& tc = registry.get<Transform3DComponent>(entity);
         auto& pc = registry.get<ParticleComponent>(entity);
 
         pc.accumulator += (float)dt;
         while (pc.accumulator > pc.interval) {
-            float n = pc.velocityNoise;
-
-            float r = Random<float>(0, pc.velocityNoise);
-            float phi = Random<float>(0, 3.142f);
-            float theta = Random<float>(0, 6.284f);
-            glm::vec3 noise = {r * std::sin(theta) * std::cos(phi), r * std::sin(theta) * std::sin(phi), r * std::cos(theta)};
-
-            spkt::particle p2;
-            p2.position = tc.position;
-            p2.velocity = pc.velocity + noise;
-            p2.acceleration = pc.acceleration;
-            p2.life = pc.life;
-            p2.scale = pc.scale;
-            emit_particle(ps, p2);
+            // Add a particle to the particle array
+            (*ps.particles)[ps.next_slot] = {
+                .position = tc.position,
+                .velocity = pc.velocity + random_noise(pc.velocityNoise),
+                .acceleration = pc.acceleration,
+                .scale = pc.scale,
+                .life = pc.life
+            };
+            ps.next_slot = --ps.next_slot % NUM_PARTICLES;
 
             pc.accumulator -= pc.interval;
         }
