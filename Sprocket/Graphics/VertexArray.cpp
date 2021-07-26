b733076abca3ae87737dbe7017b3b9a48b1a0aca
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-07-26 21:29:06 +0100

    Remove VertexArray class

--- Sprocket/Graphics/VertexArray.cpp
+++ Sprocket/Graphics/VertexArray.cpp
@@ -2,137 +2,40 @@
 
 #include <glad/glad.h>
 
+#include <concepts>
+
 namespace spkt {
+namespace {
 
-VertexArray::VertexArray()
-    : d_vao(0)
-    , d_vertex_count(0)
-    , d_instance_count(0)
+template <typename T>
+concept bindable = requires(T t)
 {
-    glGenVertexArrays(1, &d_vao);
-}
+    { t.bind() } -> std::same_as<void>;
+    { t.vertex_count() } -> std::same_as<std::size_t>;
+};
 
-VertexArray::~VertexArray()
+template <bindable T>
+void draw_impl(T* mesh, spkt::buffer<model_instance>* instances)
 {
-    glDeleteVertexArrays(1, &d_vao);
-}
-
-void VertexArray::SetModel(static_mesh* model)
-{
-    d_vertex_count = model->vertex_count();
-    if (model != nullptr) {
-        glBindVertexArray(d_vao);
-        model->bind();
-        glBindVertexArray(0);
+    mesh->bind();
+    if (instances) {
+        instances->bind();
+        glDrawElementsInstanced(GL_TRIANGLES, (int)mesh->vertex_count(), GL_UNSIGNED_INT, nullptr, instances->size());
+    } else {
+        glDrawElements(GL_TRIANGLES, (int)mesh->vertex_count(), GL_UNSIGNED_INT, nullptr);
     }
 }
 
-void VertexArray::SetModel(animated_mesh* model)
-{
-    assert(model);
-
-    d_vertex_count = model->vertex_count();
-    glBindVertexArray(d_vao);
-    model->bind();
-    glBindVertexArray(0);
-}
-
-void VertexArray::SetInstances(spkt::buffer<model_instance>* instanceData)
-{
-    if (!instanceData) {
-        d_instance_count = 0;
-        return;
-    }
-
-    d_instance_count = instanceData->size();
-    glBindVertexArray(d_vao);
-    instanceData->bind();
-    glBindVertexArray(0);
-}
-
-void VertexArray::set_model(static_mesh* model)
-{
-    d_vertex_count = model->vertex_count();
-    if (model != nullptr) {
-        model->bind();
-    }
-}
-
-void VertexArray::set_model(animated_mesh* model)
-{
-    assert(model);
-
-    d_vertex_count = model->vertex_count();
-    model->bind();
-}
-
-void VertexArray::set_instances(spkt::buffer<model_instance>* instanceData)
-{
-    if (!instanceData) {
-        d_instance_count = 0;
-        return;
-    }
-
-    d_instance_count = instanceData->size();
-    instanceData->bind();
-}
-
-void VertexArray::Draw() const
-{
-    if (d_vertex_count == 0) { return; }
-
-    glBindVertexArray(d_vao);
-    if (d_instance_count > 0) {
-        glDrawElementsInstanced(GL_TRIANGLES, (int)d_vertex_count, GL_UNSIGNED_INT, nullptr, d_instance_count);
-    }
-    else {
-        glDrawElements(GL_TRIANGLES, (int)d_vertex_count, GL_UNSIGNED_INT, nullptr);
-    }
-    glBindVertexArray(0);
-}
-
-void VertexArray::draw() const
-{
-    if (d_vertex_count == 0) { return; }
-
-    if (d_instance_count > 0) {
-        glDrawElementsInstanced(GL_TRIANGLES, (int)d_vertex_count, GL_UNSIGNED_INT, nullptr, d_instance_count);
-    }
-    else {
-        glDrawElements(GL_TRIANGLES, (int)d_vertex_count, GL_UNSIGNED_INT, nullptr);
-    }
-}
-
-void VertexArray::bind() const
-{
-    glBindVertexArray(d_vao);
-}
-
-void VertexArray::unbind() const
-{
-    glBindVertexArray(0);
 }
 
 void draw(spkt::static_mesh* mesh, spkt::buffer<model_instance>* instances)
 {
-    mesh->bind();
-    if (instances) {
-        instances->bind();
-        glDrawElementsInstanced(GL_TRIANGLES, (int)mesh->vertex_count(), GL_UNSIGNED_INT, nullptr, instances->size());
-    } else {
-        glDrawElements(GL_TRIANGLES, (int)mesh->vertex_count(), GL_UNSIGNED_INT, nullptr);
-    }
+    draw_impl(mesh, instances);
 }
 
 void draw(spkt::animated_mesh* mesh, spkt::buffer<model_instance>* instances)
 {
-    mesh->bind();
-    if (instances) {
-        instances->bind();
-        glDrawElementsInstanced(GL_TRIANGLES, (int)mesh->vertex_count(), GL_UNSIGNED_INT, nullptr, instances->size());
-    } else {
-        glDrawElements(GL_TRIANGLES, (int)mesh->vertex_count(), GL_UNSIGNED_INT, nullptr);
-    }
+    draw_impl(mesh, instances);
 }
 
 }
--- Sprocket/Graphics/VertexArray.h
+++ Sprocket/Graphics/VertexArray.h
@@ -7,34 +7,6 @@
 
 namespace spkt {
 
-class VertexArray
-{
-    std::uint32_t d_vao;
-    std::size_t d_vertex_count;
-    std::size_t d_instance_count;
-
-    VertexArray(const VertexArray&) = delete;
-    VertexArray& operator=(const VertexArray&) = delete;
-
-public:
-    VertexArray();
-    ~VertexArray();
-
-    void SetModel(spkt::static_mesh* model);
-    void SetModel(spkt::animated_mesh* model);
-    void SetInstances(spkt::buffer<model_instance>* instanceData);
-    void Draw() const;
-
-    void set_model(spkt::static_mesh* model);
-    void set_model(spkt::animated_mesh* model);
-    void set_instances(spkt::buffer<model_instance>* instanceData);
-
-    void bind() const;
-    void unbind() const;
-
-    void draw() const;
-};
-
 void draw(spkt::static_mesh* mesh, spkt::buffer<model_instance>* instances = nullptr);
 void draw(spkt::animated_mesh* mesh, spkt::buffer<model_instance>* instances = nullptr);
 
