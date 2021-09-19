1abd067d0f39c943e87ea32a55ad6bf4a58d576e
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-09-19 03:13:54 +0100

    Add lua_events files

---
+++ Sprocket/Scripting/lua_events.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <Sprocket/Scripting/LuaScript.h>
+#include <Sprocket/Utility/InputProxy.h>
+
+struct lua_State;
+
+namespace spkt {
+namespace lua {
+
+// Loads the given input_store into the scripts virtual machine, alongside a set
+// of accessor functions. It is up to the user to keep the input_store state valid.
+void load_input_store(lua::Script& script, input_store& input);
+
+}
+}
