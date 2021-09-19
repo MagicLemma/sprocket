50978fe0d305ecbf8b05138bad82a1cdefbbd428
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-09-19 03:17:43 +0100

    Remove InputProxy

--- Sprocket/Utility/InputProxy.cpp
+++ Sprocket/Utility/InputProxy.cpp
@@ -6,41 +6,6 @@
 
 namespace spkt {
 
-InputProxy::InputProxy()
-{
-    d_keyboard.fill(false);
-    d_mouse.fill(false);
-}
-  
-void InputProxy::on_event(spkt::event& event)
-{
-    if (auto data = event.get_if<keyboard_pressed_event>()) {
-        if (event.is_consumed()) { return; }
-        d_keyboard[data->key] = true;
-    }
-    else if (auto data = event.get_if<keyboard_released_event>()) {
-        d_keyboard[data->key] = false;
-    }
-    else if (auto data = event.get_if<mouse_pressed_event>()) {
-        if (event.is_consumed()) { return; }
-        d_mouse[data->button] = true;
-    }
-    else if (auto data = event.get_if<mouse_released_event>()) {
-        d_mouse[data->button] = false;
-    }
-}
-
-bool InputProxy::is_mouse_down(int button) const
-{
-    return d_mouse[button];
-}
-
-bool InputProxy::is_keyboard_down(int key) const
-{
-    assert(key < NUM_KEYS);
-    return d_keyboard[key];
-}
-
 void input_store::on_event(spkt::event& event)
 {
     if (auto data = event.get_if<keyboard_pressed_event>()) {
@@ -67,7 +32,7 @@ void input_store::end_frame()
 {
     d_keyboard_click.fill(false);
     d_keyboard_unclick.fill(false);
-    
+
     d_mouse_click.fill(false);
     d_mouse_unclick.fill(false);
 }
--- Sprocket/Utility/InputProxy.h
+++ Sprocket/Utility/InputProxy.h
@@ -8,26 +8,6 @@
 
 namespace spkt {
 
-class event;
-
-class InputProxy
-{
-public:
-    static constexpr std::size_t NUM_KEYS = 512;
-
-private:
-    std::array<bool, NUM_KEYS> d_keyboard;
-    std::array<bool, 5> d_mouse;
-
-public:
-    InputProxy();
-
-    void on_event(spkt::event& event);
-
-    bool is_mouse_down(int button) const;
-    bool is_keyboard_down(int key) const;
-};
-
 class input_store
 {
 public:
