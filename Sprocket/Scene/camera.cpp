4f16eefe6ae0a5c7bdbaad5f74fbbf4d0307ad37
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-09-28 21:46:32 +0100

    Remove the old make functions

--- Sprocket/Scene/camera.cpp
+++ Sprocket/Scene/camera.cpp
@@ -1,33 +1,17 @@
 #include "camera.h"
 
 #include <Sprocket/Graphics/Viewport.h>
-#include <Sprocket/Core/log.h>
 #include <Sprocket/Utility/Maths.h>
 
 #include <glm/glm.hpp>
 
-#include <cassert>
-
 namespace spkt {
-
-glm::mat4 spkt::make_view(const spkt::registry& registry, spkt::entity entity)
-{
-    assert((registry.has_all<Transform3DComponent, Camera3DComponent>(entity)));
-    auto [tr, cc] = registry.get_all<Transform3DComponent, Camera3DComponent>(entity);
-    return make_view(tr.position, tr.orientation, cc.pitch);
-}
 
 glm::mat4 make_view(const glm::vec3& pos, const glm::quat& ori, float pitch)
 {
     return glm::inverse(Maths::Transform(
         pos, ori * glm::rotate(glm::identity<glm::quat>(), pitch, {1, 0, 0})
     ));
-}
-
-glm::mat4 make_proj(const spkt::registry& registry, spkt::entity entity)
-{
-    assert(registry.has<Camera3DComponent>(entity));
-    return make_proj(glm::radians(registry.get<Camera3DComponent>(entity).fov));
 }
 
 glm::mat4 make_proj(float fov)
--- Sprocket/Scene/camera.h
+++ Sprocket/Scene/camera.h
@@ -1,18 +1,9 @@
 #pragma once
-#include <Sprocket/Scene/ecs.h>
+#include <glm/glm.hpp>
 
 namespace spkt {
 
-// Returns the view matrix from the entity's position.
-// If the entity has a CameraComponent, the pitch is also
-// taken into account.
-glm::mat4 make_view(const spkt::registry& registry, spkt::entity camera);
 glm::mat4 make_view(const glm::vec3& pos, const glm::quat& ori, float pitch);
-
-// Returns the projection matrix from the entity's
-// CameraComponent. If the entity does not have the component,
-// an identity matrix is returned.
-glm::mat4 make_proj(const spkt::registry& registry, spkt::entity camera);
 glm::mat4 make_proj(float fov);
     
 }
