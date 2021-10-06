106b082fa8988b7a7c4859c97744e00be0d633ef
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-10-06 18:09:24 +0100

    Add a simpler core.h file and kinematica boilerplate

--- kinematica/kinematica.m.cpp
+++ kinematica/kinematica.m.cpp
@@ -1,7 +1,27 @@
-#include <Sprocket/Core/log.h>
+#include <Sprocket/core.h>
+
+namespace km {
+
+class app
+{
+    spkt::window* d_window;
+
+public:
+    app(spkt::window* window) : d_window(window) {}
+
+    void on_update(double dt) {}
+    void on_event(spkt::event& ev) {}
+    void on_render() {}
+};
+
+}
 
 int main()
 {
-    spkt::log::info("Hello world!");
+    spkt::window window("Game");
+    km::app game(&window);
+    spkt::run_options options;
+    options.show_frame_rate = true;
+    return spkt::run(game, window, options);
     return 0;
 }
