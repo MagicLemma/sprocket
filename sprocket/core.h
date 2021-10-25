e1eddcbce503fe7cd0aa1f372bca92d48835cfef
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-10-25 01:45:11 +0100

    Update core.h

--- sprocket/core.h
+++ sprocket/core.h
@@ -1,7 +1,8 @@
 #pragma once
-#include <sprocket/Core/events.h>
-#include <sprocket/Core/game_loop.h>
-#include <sprocket/Core/input_codes.h>
-#include <sprocket/Core/log.h>
-#include <sprocket/Core/timer.h>
-#include <sprocket/Core/window.h>
+#include <sprocket/core/events.h>
+#include <sprocket/core/game_loop.h>
+#include <sprocket/core/input_codes.h>
+#include <sprocket/core/log.h>
+#include <sprocket/core/sound.h>
+#include <sprocket/core/timer.h>
+#include <sprocket/core/window.h>
